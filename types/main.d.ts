193f2bb67750b3dbd70f43b5ca20639811226cf5
Ayfri <pierre.ayfri@gmail.com>
2021-01-24 16:05:20 +0100

    Remade .gitignore.

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,18 +1,23 @@
-import * as Discord from 'discord.js';
-
-declare module 'discord-fetch-messages' {
-	/**
-	 * Fetch all the messages from a Discord Guild.
-	 * @param {module:"discord.js".Client} client - Your Discord.js Client.
-	 * @param {string} guildID - The ID of the Guild to be fetch.
-	 * @returns {Promise<module:"discord.js".Message[]>} - All the messages fetched.
-	 */
-	export function fetchGuildMessages(client: Discord.Client, guildID: Discord.Snowflake): Promise<Discord.Message[]>;
-	/**
-	 * Fetch all the messages from a Discord TextChannel.
-	 * @param {module:"discord.js".Client} client - Your Discord.js Client.
-	 * @param {module:"discord.js".TextChannel | module:"discord.js".NewsChannel} channel - The ID of the Discord TextChannel.
-	 * @returns {Promise<module:"discord.js".Message[]>} - All the messages fetched.
-	 */
-	export function fetchChannelMessages(client: Discord.Client, channel: Discord.Channel): Promise<Discord.Message[]>;
-}
+/**
+ * Fetch all the messages from a Discord Guild.
+ * @param {module:"discord.js".Client} client - Your Discord.js Client.
+ * @param {string} guildID - The ID of the Guild to be fetch.
+ * @returns {Promise<module:"discord.js".Message[]>} - All the messages fetched.
+ * @deprecated Use {@link Fetcher} class instead.
+ */
+export function fetchGuildMessages(client: any, guildID: string): Promise<{
+    "\"C:/Users/Pierre/WebstormProjects/libs/discord-fetch/src/main\"": typeof import("./main");
+}>;
+/**
+ * Fetch all the messages from a Discord TextChannel.
+ *
+ * @param {module:"discord.js".Client} client - Your Discord.js Client.
+ * @param {module:"discord.js".TextChannel | module:"discord.js".NewsChannel} channel - The ID of the Discord TextChannel.
+ * @returns {Promise<module:"discord.js".Message[]>} - All the messages fetched.
+ * @deprecated Use {@link Fetcher} class instead.
+ */
+export function fetchChannelMessages(client: any, channel: any): Promise<{
+    "\"C:/Users/Pierre/WebstormProjects/libs/discord-fetch/src/main\"": typeof import("./main");
+}>;
+declare namespace __C__Users_Pierre_WebstormProjects_libs_discord_fetch_src_main_ { }
+export {};
